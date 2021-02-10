7bd2dd08da66782d44351ac3a1d321acd6fcf309
class02 <class02@Class02T01.mshome.net>
2021-02-10 18:27:00 +0900

    day18 db last commit

--- db/sql/day18.sql
+++ db/sql/day18.sql
@@ -428,18 +428,344 @@ END;
 exec get_dcount(10);
 exec get_dcount(40);
 
+------------------------------------------------------------------
 /*
-
+    트리거(TRIGGER)
+    ==> DML 질의명령을 실행하면
+        자동적으로 다른 처리를 하고자 할 경우에 
+        사용하는 프로시저의 일종
+        
+    회원탈퇴를 할 경우
+    회원테이블에서 데이터를 바로 삭제하기 전에
+    기존데이터를 미리 저장해놓는 작업이 선행이 되어지면
+    데이터베이스 사용측면이나 삭제작업에서도
+    편해질 것이다.
+    
+    회원가입을 하게되면 회원의 등급을 주는 경우
+    이 회원의 등급을 만들어줘야 한다.
+    회원정보를 관리하는 테이블과 등급을 관리하는 테이블이 다르면
+    두번 입력작업을 해줘야 한다.
+    
+    이처럼 작업을 하다보면 관련된(서로 연결된) 작업이
+    진행되어야 하는 경우가 빈번히 발생한다.
+    이때 한가지 작업만 하면 다른 작업이 자동적으로 처리되도록 하고자 할 경우
+    사용하는 프로시저를 트리거라고 한다.
+    
+    트리거 만드는 형식 ]
+        
+        CREATE OR REPLACE TRIGGER 트리거이름
+            BEFORE 또는 AFTER     DML명령(INSERT, UPDATE, DELETE)이름 
+            
+            설명 ]
+                입력 작업의 경우
+                
+                    BEFORE  - 입력작업이 일어나기 직전에 실행되는 트리거
+                    AFTER   - 입력작업이 완료된 후 실행되는 트리거
+            
+        ON
+            테이블이름 
+            
+        [FOR EACH ROW]
+            ==> 생략하면 작업단위를 하나로 처리한다.
+                예를들자면
+                    DELETE FROM tmp2 WHERE deptno = 10;
+                    명령을 실행하면 3개의 데이터가 삭제되는데
+                    이때 3개의 삭제작업을 하나로 처리해서
+                    3개의 작업이 완료된후 트리거가 실행되도록 한다.
+                    
+                생략하지 않으면 위의 질의명령의 경우
+                각가의 사원들이 삭제될때마다 트리거가 실행된다.
+        [WHEN
+            조건식]
+            ==> 트리거가 발생해야 하는 조건식을 지정할 수 있다.
+                
+                예 ]
+                    WHEN
+                        deptno = 10
+                    이라고 하면 부서번호가 10번인 내용이 변경되면
+                    그때만 트리거를 실행하라는 의미가 된다.
+        BEGIN
+            -- 트리거가 실행해야할 질의명령을 기술하는 부분.
+        END;
+        /
+    
+참고 ]
+    
+    트리거가 발생하면 자동적으로 변수가 두개 발생한다.
+        
+        :OLD    - 이전 데이터를 기억하는 변수
+        :NEW    - 이후 데이터를 기억하는 변수
+        ==> 이것은 ROWTYPE 변수로 해당 테이블의 필드를 멤버변수로 가지는 변수
+        
+        예 ]
+            :OLD.ename
+            ==> 실행할 명령 이전의 ename 데이터를 기억
+            
+            :NEW.ename
+            ==>  실행 명령 이후의 ename 데이터를 기억
+            
+        주의 ]
+            
+            DELETE 명령의 경우
+            :OLD.ename은 존재하지만 :NEW.ename 은 존재하지 않는다.
+            
+            INSERT 명령의 경우
+            :OLD.ename 은 존재하지 않는다.
+            :NEW.ename은 존재한다.
+            
+            update 명령의 경우는 둘 모두 데이터가 존재한다.
+    
+        참고 ]
+             만약 FOR EACH ROW 를 생략하면
+             :NEW, :OLD 변수를 사용할 수 없다.
+             이유는
+                데이터가 몇개 변경될 수 있는지 알 수 없으므로...
+                어떤 데이터를 사용할지 알 수 없기 때문에...
 */
 
+-- 벡업테이블 준비
+drop table emp_backup;
+
+CREATE TABLE emp_backup
+AS
+    SELECT
+        t.*, sysdate firedate
+    FROM
+        tmp2 t
+    WHERE
+        1 = 2
+;
+
+ALTER TABLE emp_backup
+RENAME CONSTRAINT SYS_C0011581 TO EBACKUP_NAME_NN;
+
+ALTER TABLE emp_backup
+ADD
+    CONSTRAINT EBACKUP_NO_PK PRIMARY KEY(empno);
+    
+ALTER TABLE emp_backup
+MODIFY
+    firedate DEFAULT sysdate;
+
+/*
+    tmp2 테이블에서 작업
+    사원이 퇴사를 하면 
+    벡업테이블에 데이터를 저장하는 트리거(femp01)를 작성하세요.
+    
+    그리고 부서번호가 10인 사원들을 모두 퇴사처리하세요.
+*/
+
+-- DELETE 명령의 경우는 사원 퇴사처리가 되기전에 데이터를 벡업해둬야한다.
+-- 그래야 데이터를 벡업할 수 있으니까...
+CREATE OR REPLACE TRIGGER femp01
+    BEFORE DELETE
+ON
+    tmp2
+FOR EACH ROW
+BEGIN
+    INSERT INTO
+        emp_backup(empno, ename, job, mgr, hiredate, sal, comm, deptno)
+    VALUES(
+        :old.empno, :old.ename, :old.job, :old.mgr, 
+        :old.hiredate, :old.sal, :old.comm, :old.deptno
+    );
+    
+    DBMS_OUTPUT.PUT_LINE(:OLD.ename || ' ]  사원 퇴사 처리가 완료되었습니다.');
+END;
+/
+
+SELECT
+    *
+FROM
+    tmp2
+WHERE
+    deptno = &deptno
+;
+
+select 'abcd', &age from dual;
+
+DELETE FROM
+    tmp2
+WHERE
+    deptno = 10
+;
+
+commit;
+
+DELETE FROM
+    tmp2
+WHERE
+    deptno = 20
+;
+
+ROLLBACK;
+
+/*
+    문제 ]
+        사원이 입사하면
+        해당 사원의 사원번호와 사원이름을 입사 확인 메세지를 출력해주는
+        트리거(addemp01)를 작성하고 실행하세요.
+        
+        출력형식 ]
+            
+            0000 ] 번 [ XXXXXX ] 사원이 입사했습니다.
+*/
+
+CREATE OR REPLACE TRIGGER addemp01
+    AFTER INSERT
+ON
+    tmp2
+FOR EACH ROW
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(   :NEW.empno ||' ] 번 [ ' || 
+                            :NEW.ename || ' ] 사원이 입사했습니다.');
+END;
+/
 
 
+INSERT INTO
+    tmp2(empno, ename)
+VALUES(
+    9000, 'DOOLY'
+);
 
+--------------------------------------------------------------------------------
+/*
+    스케줄러
+    ==> 특정 시간이 되면 원하는 질의가 자동 실행되도록 하는 기능을 이야기한다.
+        즉, 오라클에게 스케줄을 알려주어서 
+        그 스케줄대로 작업을 실행하라고 해서
+        스케줄러가로 한다.
+        
+        
+    작업 순서 ]
+        
+        1. 실행할 질의명령을 포함하는 프로시저를 만들어 놓는다.
+        2. 프로그램 등록
+            ==> 스케줄러가 실행할 프로그램(1번에서 만든 프로시저)를 등록해 줘야 한다.
+            
+            형식 ]
+                BEGIN
+                    DBMS_SCHEDULER.CREATE_PROGRAM(
+                        program_name => '프로그램이름',
+                        program_action => '사용할 프로시저이름',
+                        -- 프로시저를 이용하지 않고 
+                        -- 직접 질의명령을 만들어서 사용해도 무방하다.
+                        program_type    => 'STORED_PROCEDURE',
+                        -- STORED_PROCEDURE : 저장프로시저를 사용하는 경우
+                        -- EXECUTABLE       : 프로시저를 사용하지 않고 질의며령을 기술하는 경우
+                        comments    => '설명',
+                        enabled     => 'TRUE'
+                        -- TRUE : 만드는 순간 활성화하세요.
+                    );
+                END;
+                /
+        
+        3. 스케줄을 등록
+            ==> 언제 프로시저가 실행될지를 지정하는 내용을 만드는 것
+            
+            형식 ]
+                BEGIN
+                    DBMS_SCHEDULER.CREATE_SCHEDULE(
+                        schedule_name   => '스케줄이름',
+                        start_date  => 시작 날짜,
+                        end_date    =>  종료날짜,
+                        repeat_interval => 반복주기,
+                        comments => '설명'
+                    );
+                END;
+                /
+                
+            참고 ]
+                start_date, end_date는 생략해도 무방하다.
+                start_date 를 생략하면 만드는 순간부터 스케줄이 가동하도록한다.
+                end_date 를 생략하면 무한정 반복한다.
+                
+                repeat_interval 은 반복 주기를 지정하는 부분
+                
+                형식 ]
+                    repeat_interval => FREQ=?1?;INVERVAL=?2? 의 형식으로 지정한다.
+                    
+                    ?1?     간격을 의미한다.
+                        예]
+                            HOURLY      - 시간간격
+                            MINUTELY    - 분 간격
+                            DAILY       - 일간격
+                            
+                    ?2?     간격주기
+                    
+                    repeat_interval => FREQ=MINUTELY;INTERVAL=1
+                    ==> 1분 간격으로 실행하세요...
+            
+        4. 잡 등록
+            ==> 2번의 내용과 3번 내용을 합쳐서 하나의 스케줄 작업을 완성하는 단계
+            
+            형식 ]
+                BEGIN
+                    DBMS_SCHEDULER.CREATE_JOB(
+                        job_name => '잡이름',
+                        program_name => '프로그램이름',
+                        schedule_name => '스케줄이름',
+                        comments => '설명'
+                    );
+                END;
+                /
+                
+    삭제하기
+        execute DBMS_SCHEDULER.DROP_JOB('잡이름', false);
+        execute DBMS_SCHEDULER.DROP_PROGRAM('프로그램이름', false);
+        execute DBMS_SCHEDULER.DROP_SCHEDULE('스케줄이름', false);
+*/
 
+CREATE TABLE tmp3(
+    no NUMBER(4),
+    wdate DATE default sysdate
+);
 
+/*
+    tmp3 테이블에 1분 마다 자동으로 데이터가 채워지도록 
+    스케줄러를 작성해서 실행하세요.
+*/
 
-
-
-
-
-
+-- 1. 프로시저를 만든다.
+CREATE OR REPLACE PROCEDURE adddata01
+IS
+BEGIN
+    INSERT INTO
+        tmp3(no)
+    VALUES(
+        (SELECT NVL(MAX(no) + 1, 1) FROM tmp3)
+    );
+END;
+/
+-- 2. 프로그램 등록
+BEGIN
+    DBMS_SCHEDULER.CREATE_PROGRAM(
+        program_name => 'addsch00',
+        program_action => 'adddata01',
+        program_type    => 'STORED_PROCEDURE',
+        comments    => 'test 프로그램',
+        enabled     => TRUE
+    );
+END;
+/
+-- 3. 스케줄 등록
+BEGIN
+    DBMS_SCHEDULER.CREATE_SCHEDULE(
+        schedule_name => 'jom01',
+        repeat_interval => 'FREQ=MINUTELY;INTERVAL=1',
+        comments => 'just one minute'
+    );
+END;
+/
+-- 4. 잡 등록
+BEGIN
+    DBMS_SCHEDULER.CREATE_JOB(
+        job_name => 'job01',
+        program_name => 'addsch00',
+        schedule_name => 'jom01',
+        comments => '분마다 데이터 입력 스케줄러',
+        enabled => TRUE
+    );
+END;
+/
